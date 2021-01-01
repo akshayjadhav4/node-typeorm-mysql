1eda2da60df7a7c3fa47c61dd3726d12b918abbf
AKSHAY <akshayjadhav461999@gmail.com>
2021-01-01 12:20:40 +0530

    running express first time

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Express Setup in progress...");
+import * as express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from / route");
+});
+
+const PORT = 2004;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
