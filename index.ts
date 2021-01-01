d4e718157c638fc8afe543f530e7bf0f1555d4f2
AKSHAY <akshayjadhav461999@gmail.com>
2021-01-01 14:13:00 +0530

    created connection with db

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import * as express from "express";
 import routes from "./src/routes";
+import { createConnection } from "typeorm";
 
-const app = express();
-app.use(express.json());
+createConnection()
+  .then((connection) => {
+    const app = express();
+    app.use(express.json());
 
-app.use(routes);
+    app.use(routes);
 
-const PORT = 2004;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+    const PORT = 2004;
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("ERROR WHILE CONNECTING DB ", error);
+  });
